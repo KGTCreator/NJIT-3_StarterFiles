ab75b0227a8027a517b6473207ba65159ccb9d30
KGTCreator <524Ktobin@frhsd.com>
2024-05-16 13:38:13 -0400

    The Final Stretch

--- site.js
+++ site.js
@@ -40,15 +40,62 @@ const vue_app = Vue.createApp({
       }
     },
       methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-      },
-      title: {
-            
-      },
-      owner: {
 
+            
+            getMonthText(dateArray) {
+                  var year = dateArray[0]
+                  var day = dateArray[2]
+                  let month
+                  switch(dateArray[1]){
+                        case 1:
+                              month = 'January'
+                              break;
+                        case 2:
+                              month = 'Febuary'
+                              break;
+                        case 3:
+                              month = 'March'
+                              break;
+                        case 4:
+                              month = 'April'
+                              break;
+                        case 5:
+                              month = 'May'
+                              break;
+                        case 6:
+                              month = 'June'
+                              break;
+                        case 7:
+                              month = 'July'
+                              break;
+                        case 8:
+                              month = 'August'
+                              break;
+                        case 9:
+                              month = 'September'
+                              break;
+                        case 10:
+                              month = 'October'
+                              break;
+                        case 11:
+                              month = 'November'
+                              break;
+                        case 12:
+                              month = 'December'
+                              break;
+                  }                
+            return month + ' ' + day + ', ' + year[0]
+            },
+            like(index) {
+                 this.like++
+            },
+            dislike(index) {
+                  this.dislike++
+            },
+         
+           
       },
-      github
+      
 })
 
 vue_app.mount("#vue_app")
