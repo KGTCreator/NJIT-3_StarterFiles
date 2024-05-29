879da1989a7f517e122ddb848b34086935d5e3ee
KGTCreator <524Ktobin@frhsd.com>
2024-05-29 14:33:13 -0400

    Thank You, Brandon

--- site.js
+++ site.js
@@ -39,61 +39,63 @@ const vue_app = Vue.createApp({
          
       }
     },
-      methods: {
-
-            
-            getMonthText(dateArray) {
-                  var year = dateArray[0]
-                  var day = dateArray[2]
-                  let month
-                  switch(dateArray[1]){
-                        case 1:
-                              month = 'January'
-                              break;
-                        case 2:
-                              month = 'Febuary'
-                              break;
-                        case 3:
-                              month = 'March'
-                              break;
-                        case 4:
-                              month = 'April'
-                              break;
-                        case 5:
-                              month = 'May'
-                              break;
-                        case 6:
-                              month = 'June'
-                              break;
-                        case 7:
-                              month = 'July'
-                              break;
-                        case 8:
-                              month = 'August'
-                              break;
-                        case 9:
-                              month = 'September'
-                              break;
-                        case 10:
-                              month = 'October'
-                              break;
-                        case 11:
-                              month = 'November'
-                              break;
-                        case 12:
-                              month = 'December'
-                              break;
-                  }                
-            return month + ' ' + day + ', ' + year[0]
-            },
-           
-            posterClick(index) {
-               
-            }
-         
-           
+    methods: {
+      getMonthText(dateArray) {
+          const [year, month, day] = dateArray;
+          let monthName;
+          switch (month) {
+              case 1:
+                  monthName = "January";
+                  break;
+              case 2:
+                  monthName = "February";
+                  break;
+              case 3:
+                  monthName = "March";
+                  break;
+              case 4:
+                  monthName = "April";
+                  break;
+              case 5:
+                  monthName = "May";
+                  break;
+              case 6:
+                  monthName = "June";
+                  break;
+              case 7:
+                  monthName = "July";
+                  break;
+              case 8:
+                  monthName = "August";
+                  break;
+              case 9:
+                  monthName = "September";
+                  break;
+              case 10:
+                  monthName = "October";
+                  break;
+              case 11:
+                  monthName = "November";
+                  break;
+              case 12:
+                  monthName = "December";
+                  break;
+              default:
+                  monthName = "Invalid Month";
+          }
+          return `${monthName} ${day}, ${year}`;
       },
-      
-})
+      posterClick(index) {
+          const movie = this.movies[index];
+          movie.posterindex = (movie.posterindex + 1) % movie.posters.length;
+      },
+      incrementLikes(index) {
+          this.movies[index].likes++;
+      },
+      incrementDislikes(index) {
+          this.movies[index].dislikes++;
+      }
+  }
+});
 
 vue_app.mount("#vue_app")
